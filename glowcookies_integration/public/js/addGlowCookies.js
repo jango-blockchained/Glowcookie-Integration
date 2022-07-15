2c011c4c323ee28310d1afffeed91350e46d59d8
jango_blockchained <jan.goischke@gmail.com>
2022-07-15 00:34:08 +0000

    Added build.json

--- glowcookies_integration/public/js/addGlowCookies.js
+++ glowcookies_integration/public/js/addGlowCookies.js
@@ -1,20 +1,10 @@
-
-let gc_options = {};
-
-frappe.db.get_single_value('GlowCookies Settings','active')
-	.then(active => {
-		if (active) {
-			frappe.db.get_single_value('GlowCookies Settings','language_flag')
-				.then(language_flag => {gc_options.language_flag = language_flag});
-			frappe.db.get_single_value('GlowCookies Settings','style')
-				.then(style => {gc_options.style = style});
-			frappe.db.get_single_value('GlowCookies Settings','policy_url')
-				.then(policy_url => {gc_options.policy_url = policy_url});
-			// --
-			glowCookies.start(gc_options.language_flag, { 
-				style: gc_options.style,
-				policyLink: gc_options.policy_url
-			});
-		}
+frappe.call('glowcookies_integration.api.utils.getSettings')
+.then(r => {
+  	let gc_opt = r.message;
+	if (gc_opt.active) {
+		glowCookies.start(gc_opt.language_flag, { 
+				style: gc_opt.style,
+				policyLink: gc_opt.policy_url
+		});
 	}
-);
+});
