391a3e1b7c1a7c9e427c3b5dc7a425ec9c81b113
jango_blockchained <jan.goischke@gmail.com>
2022-07-15 00:55:37 +0000

    Added auto minify

--- glowcookies_integration/api/utils.py
+++ glowcookies_integration/api/utils.py
@@ -1,17 +1,8 @@
 from __future__ import unicode_literals
-from frappe import _
-
 from frappe.utils import get_site_name
 
 import frappe
-import json
-
-# ------ INIT FRAPPE4
-frappe.init(site=get_site_name)
-frappe.connect()
-
 
 @frappe.whitelist()
 def getSettings():
-    settings = frappe.get_doc('GlowCookies Settings')
-    return settings
+    return frappe.get_doc('GlowCookies Settings')
