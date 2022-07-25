af09aaa7f9db9b3c409788fc8bc2b5f6d43fe743
jango_blockchained <jan.goischke@gmail.com>
2022-07-25 18:32:43 +0000

    CSFR Token conform

--- glowcookies_integration/public/js/glowCookies.js
+++ glowcookies_integration/public/js/glowCookies.js
@@ -485,30 +485,19 @@ class LanguagesGC {
 
 const glowCookies = new GlowCookies()
 
-// frappe.call('glowcookies_integration.api.utils.getSettings')
-// .then(r => {
-//   	let gc_opt = r.message;
-// 	if (gc_opt.active) {
-// 		glowCookies.start(gc_opt.language_flag, { 
-// 				style: gc_opt.style,
-// 				policyLink: gc_opt.policy_url
-// 		});
-// 	}
-// });
-// glowCookies.start('de', { 
-// 				style: 2,
-// 				policyLink: 'https://erp.abigruppe.de/datenschutz'
-// 		});
-
-frappe.call('glowcookies_integration.api.utils.getSettings')
+/**
+ * @author: jango_blockchained 
+ */
+const xtenderCookie = xcall('glowCookiesSettingsWrapper')
 .then(r => {
-  let gc_opt = r.message;
-	if (gc_opt.active === 1) {
-		glowCookies.start(gc_opt.language_flag, { 
-			style: gc_opt.style,
-			policyLink: gc_opt.policy_url
-		});
-	} else {
-		console.log('inactive cookie', gc_opt.active)
-	}
+  // console.log( "pending...", r);
+  let res = r || r.message;
+  if (res.active) {
+    glowCookies.start(res.language_flag, { 
+      style: res.style,
+      policyLink: res.policy_url
+    });
+  } else {
+    console.log('inactive cookie', res.active)
+  }
 });
